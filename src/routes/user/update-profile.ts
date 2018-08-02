123e6c3518fc8de68955af2b4ef62402a91a088c
Varenic Victor <victorv@touchpanelcontrol.com>
2018-08-02 15:26:55 +0300

    update profile

--- src/routes/user/update-profile.ts
+++ src/routes/user/update-profile.ts
@@ -5,7 +5,6 @@ export default async (req, res, next) => {
     const id = req.params.id;
 
     req.body.birthday = moment(req.body.birthday).unix();
-    console.log(req.body);
     const user = await Profile
         .query()
         .update(req.body)
