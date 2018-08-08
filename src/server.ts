6091afc256aa256babccad0b62d6736f746d189e
Varenic Victor <victorv@touchpanelcontrol.com>
2018-08-08 19:21:20 +0300

    -

--- src/server.ts
+++ src/server.ts
@@ -14,16 +14,11 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws: WebSocket) => {
 
-    //connection is up, let's add a simple simple event
     ws.on('message', (message: string) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        ws.send(JSON.stringify({ text: 'ok' }));
     });
 
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send(JSON.stringify({ text: 'Welcome to chat!' }));
 });
 
 app.set("x-powered-by", false);
