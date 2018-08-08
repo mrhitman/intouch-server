21ee95c56035bfe8df8e76801bdc45773deb2fc9
Varenic Victor <victorv@touchpanelcontrol.com>
2018-08-08 16:13:41 +0300

    chat

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,26 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as passport from 'passport';
 import * as morgan from 'morgan';
+import * as WebSocket from 'ws';
+import * as http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 app.set("x-powered-by", false);
 app.use(cors());
@@ -22,12 +40,13 @@ app.use('/friend', require('./routes/friend').default);
 app.get('//', (_, res) => {
     res.json('ok');
 });
+
 app.use((err, req, res, next) => {
     res.status(400);
     res.json({ error: err.message });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('server up'));
+server.listen(process.env.PORT || 3000, () => console.log('server up'));
 
 process.on("unhandledRejection", (reason, p) => {
     global.console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
