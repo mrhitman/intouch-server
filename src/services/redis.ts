fbefdad4a781f9f3e6d44e608d77fa66da6e1047
Varenic Victor <victorv@touchpanelcontrol.com>
2018-08-16 12:58:49 +0300

    redis env

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -2,7 +2,7 @@ import { forEach } from 'lodash';
 import * as redis from 'redis';
 import { promisify } from 'util';
 
-const client = redis.createClient(process.env.REDIS_HOST, process.env.REDIS_PORT);
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 const map = {
     get: client.get,
     set: client.set,
