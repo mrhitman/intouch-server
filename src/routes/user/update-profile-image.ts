6338bc44f84871493acd8dfde542df44b15b0bdb
Varenic Victor <victorv@touchpanelcontrol.com>
2018-08-20 19:14:10 +0300

    upload image

--- src/routes/user/update-profile-image.ts
+++ src/routes/user/update-profile-image.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { promisify } from 'util';
 import { Profile } from '../../models/profile';
+import { info, crop } from 'easyimage';
 
 const unlink = promisify(fs.unlink);
 const access = promisify(fs.access);
@@ -20,8 +21,8 @@ export default async (req, res) => {
             .status(404)
             .send('No such user found');
     }
-
-    let photo = req.files.photo;
+    const { x, y, width, height } = req.body;
+    const photo = req.files.photo;
     try {
         if (await access(`images/${profile.photo}`, fs.constants.F_OK)) {
             await unlink(`images/${profile.photo}`);
@@ -29,7 +30,16 @@ export default async (req, res) => {
         await profile
             .$query()
             .update({ photo: `profile_${profile.user_id}.png` });
-        await photo.mv(`images/${profile.photo}`);
+        await photo.mv(`images/origin_${profile.photo}`);
+        const imageInfo = await info(`images/origin_${profile.photo}`);
+        await crop({
+            src: `images/origin_${profile.photo}`,
+            dst: `images/${profile.photo}`,
+            x: (x * imageInfo.width) / 100,
+            y: (y * imageInfo.height) / 100,
+            cropHeight: (height * imageInfo.width) / 100,
+            cropWidth: (width * imageInfo.height) / 100,
+        });
         res.json('success');
     } catch (err) {
         return res
